f308b7f20d2704998f27c4eea2ae84e391b37b0f
Mari Vignesh <marivignesh242004@gmail.com>
2026-07-20 00:03:30 +0530

    Add All files

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import Header from './components/Header';
 import HeroBanner from './components/HeroBanner/HeroBanner';
 import ContactSection from './components/ContactSection/ContactSection';
 import Footer from './components/Footer';
+import ProductsSection from './components/ProductsSection/ProductsSection';
+import ServicesSection from './components/ServicesSection/ServicesSection';
+import BrandingMarketingPage from './pages/BrandingMarketingPage';
+import CareersApplyPage from './pages/CareersApplyPage';
+import MobileAppDesignPage from './pages/MobileAppDesignPage';
+import UiUxDesignPage from './pages/UiUxDesignPage';
+import VideoEditingPage from './pages/VideoEditingPage';
+import WebDevelopmentPage from './pages/WebDevelopmentPage';
 import { prefersReducedMotion } from './utils/motionPreference';
 import './App.css';
 
@@ -16,6 +24,9 @@ function HomeSections() {
       <HeroBanner />
       <AboutSection />
       <ServicesSection />
+      <ProductsSection />
+      <CareersSection />
+      <ContactSection />
     </>
   );
 }
@@ -43,11 +54,9 @@ function App() {
             <Route path="/services/mobile-app-design" element={<MobileAppDesignPage />} />
             <Route path="/services/video-editing" element={<VideoEditingPage />} />
             <Route path="/services/branding-marketing" element={<BrandingMarketingPage />} />
+            <Route path="/careers/apply" element={<CareersApplyPage />} />
             <Route path="*" element={<HomeSections />} />
           </Routes>
-          <HeroBanner />
-          <AboutSection />
-          <ContactSection />
         </main>
         <Footer />
       </div>
