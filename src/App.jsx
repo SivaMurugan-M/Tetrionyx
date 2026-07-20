265bf91f23e0fce754cbae6329ebb9173aee123b
SivaMurugan-M <sivamurugan04012004@gmail.com>
2026-07-20 22:02:51 +0530

    Merge main into mari

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import AboutSection from './components/AboutSection/AboutSection';
 import BrandIntro from './components/BrandIntro';
 import CareersSection from './components/CareersSection/CareersSection';
+import ContactSection from './components/ContactSection/ContactSection';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import HeroBanner from './components/HeroBanner/HeroBanner';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import ServicesSection from './components/ServicesSection/ServicesSection';
-=======
-import ContactSection from './components/ContactSection/ContactSection';
-import Footer from './components/Footer';
-<<<<<<< HEAD
->>>>>>> f308b7f20d2704998f27c4eea2ae84e391b37b0f
 import ProductsSection from './components/ProductsSection/ProductsSection';
 import ServicesSection from './components/ServicesSection/ServicesSection';
 import BrandingMarketingPage from './pages/BrandingMarketingPage';
-<<<<<<< HEAD
-=======
-import ContactSection from './components/ContactSection/ContactSection';
-import Footer from './components/Footer';
->>>>>>> main
-=======
 import CareersApplyPage from './pages/CareersApplyPage';
-=======
-import ProductsSection from './components/ProductsSection/ProductsSection';
-import ServicesSection from './components/ServicesSection/ServicesSection';
-import BrandingMarketingPage from './pages/BrandingMarketingPage';
->>>>>>> main
 import MobileAppDesignPage from './pages/MobileAppDesignPage';
 import UiUxDesignPage from './pages/UiUxDesignPage';
 import VideoEditingPage from './pages/VideoEditingPage';
 import WebDevelopmentPage from './pages/WebDevelopmentPage';
-<<<<<<< HEAD
->>>>>>> f308b7f20d2704998f27c4eea2ae84e391b37b0f
-=======
->>>>>>> main
 import { prefersReducedMotion } from './utils/motionPreference';
 import './App.css';
 
@@ -45,21 +24,9 @@ function HomeSections() {
       <HeroBanner />
       <AboutSection />
       <ServicesSection />
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <ProductsSection />
-      <CareersSection />
-<<<<<<< HEAD
-=======
->>>>>>> main
-=======
-      <ContactSection />
->>>>>>> f308b7f20d2704998f27c4eea2ae84e391b37b0f
-=======
       <ProductsSection />
       <CareersSection />
       <ContactSection />
->>>>>>> main
     </>
   );
 }
@@ -87,20 +54,9 @@ function App() {
             <Route path="/services/mobile-app-design" element={<MobileAppDesignPage />} />
             <Route path="/services/video-editing" element={<VideoEditingPage />} />
             <Route path="/services/branding-marketing" element={<BrandingMarketingPage />} />
-<<<<<<< HEAD
             <Route path="/careers/apply" element={<CareersApplyPage />} />
             <Route path="*" element={<HomeSections />} />
           </Routes>
-<<<<<<< HEAD
-          <HeroBanner />
-          <AboutSection />
-          <ContactSection />
-=======
->>>>>>> f308b7f20d2704998f27c4eea2ae84e391b37b0f
-=======
-            <Route path="*" element={<HomeSections />} />
-          </Routes>
->>>>>>> main
         </main>
         <Footer />
       </div>
@@ -113,4 +69,3 @@ function App() {
 }
 
 export default App;
-
